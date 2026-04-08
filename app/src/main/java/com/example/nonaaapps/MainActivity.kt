fedcd2f0f73da396218760e1dd8f8f055c03901a
nona mobile <edith24si@mahasiswa.pcr.ac.id>
2026-04-08 08:28:25 +0700

    p4_pertama

--- app/src/main/java/com/example/nonaaapps/MainActivity.kt
+++ app/src/main/java/com/example/nonaaapps/MainActivity.kt
@@ -1,20 +1,42 @@
 package com.example.nonaaapps
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.nonaaapps.databinding.ActivityMainBinding
+import com.example.nonaaapps.databinding.ActivityThirdBinding
+import com.example.nonaaapps.pertemuan4.FourthActivity
+import com.example.nonaaapps.pertemuan_3.ThirdResultActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        binding.btnToFourth.setOnClickListener {
+
+            val intent = Intent(this, FourthActivity::class.java)
+            intent.putExtra("name", "Politeknik Caltex Riau")
+            intent.putExtra("from", "Rumbai")
+            intent.putExtra("age", 25)
+
+            startActivity(intent)
+
+
+
+
+        }
+
     }
 }
